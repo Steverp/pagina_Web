c7988c83263353168ee275b87a0419e6576b5c3f
Kevin Romero <Kevin Romero>
2016-11-23 22:53:50 -0500

    Agregadas Librerias css js

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>Inicio</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+        <link href="resources/css/mystyles.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmenu" aria-expanded="false" aria-controls="narbar">
+                        <span class="sr-only">Boton despliega</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Inferno Technologies</a>
+                </div>
+                <div class="navbar-collapse collapsed" id="navmenu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Entradas</a></li>
+                        <li><a href="#">Acerca De</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+       <!-- Llamando JQuery -->
+        <script src="resources/js/jquery.min.js"></script>
+        <script src="resources/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
